57a1b314fc0a7a327f03b1eabcf4d5d6b472a7b1
A <qwertyuiop5483@gmail.com>
2020-08-10 11:09:36 -0400

    Initial commit of OS practice
    Bios > MBR > Loader

---
+++ run.sh
@@ -0,0 +1,11 @@
+set -x
+
+./bin/bximage -hd -mode="flat" -size=60 -q hd60M.img
+
+nasm -I ./src/include/ -o /tmp/mbr.bin ./src/mbr.S
+dd if=/tmp/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+
+nasm -I ./src/include/ -o /tmp/loader.bin ./src/loader.S
+dd if=/tmp/loader.bin of=hd60M.img bs=512 count=1 seek=2 conv=notrunc
+
+./bin/bochs -f bochsrc.disk
