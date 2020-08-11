24809e75d54fefc3b50ace0342b4dd037ebb3ba3
A <qwertyuiop5483@gmail.com>
2020-08-11 13:41:35 -0400

    Switch to protected mode from real mode

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ nasm -I ./src/include/ -o /tmp/mbr.bin ./src/mbr.S
 dd if=/tmp/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 
 nasm -I ./src/include/ -o /tmp/loader.bin ./src/loader.S
-dd if=/tmp/loader.bin of=hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=/tmp/loader.bin of=hd60M.img bs=512 count=200 seek=2 conv=notrunc
 
 ./bin/bochs -f bochsrc.disk
